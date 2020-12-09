6936a2ac6cb4fdadd29ae736d583aac6d2ff7d3a
Viktor <61415746+VS-web-developer@users.noreply.github.com>
2020-12-09 14:31:38 +0300

    Add files
    Add files

---
+++ js/app.js
@@ -0,0 +1,34 @@
+
+new hoverEffect({
+	parent: document.querySelector('.distortion-ph1'),
+	intensity: 0.2,
+	image1: '../src/img/ph1.jpg',
+	image2: '../src/img/ph1-2.jpg',
+	displacementImage: '../src/img/diss.png',
+	imagesRatio: 0.7
+})
+
+new hoverEffect({
+	parent: document.querySelector('.distortion-ph4'),
+	intensity: 0.2,
+	image1: '../src/img/ph2.jpg',
+	image2: '../src/img/ph2-2.jpg',
+	displacementImage: '../src/img/ramen.jpg',
+	imagesRatio: 1.2
+})
+new hoverEffect({
+	parent: document.querySelector('.distortion-ph2'),
+	intensity: 0.2,
+	image1: '../src/img/ph3.jpg',
+	image2: '../src/img/ph3-2.jpg',
+	displacementImage: '../src/img/stripe1.png',
+	imagesRatio: 0.7
+})
+new hoverEffect({
+	parent: document.querySelector('.distortion-ph3'),
+	intensity: 0.2,
+	image1: '../src/img/ph4.jpg',
+	image2: '../src/img/ph4-2.jpg',
+	displacementImage: '../src/img/fluid.jpg',
+	imagesRatio: 1.2
+})
