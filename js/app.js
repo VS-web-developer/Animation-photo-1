6997806a3dd4eab1fe7c6aceaf6835358f220a1a
Viktor <61415746+VS-web-developer@users.noreply.github.com>
2020-12-09 14:48:44 +0300

    change path2

--- js/app.js
+++ js/app.js
@@ -2,33 +2,33 @@
 new hoverEffect({
 	parent: document.querySelector('.distortion-ph1'),
 	intensity: 0.2,
-	image1: '../img/ph1.jpg',
-	image2: '../img/ph1-2.jpg',
-	displacementImage: '../img/diss.png',
+	image1: 'img/ph1.jpg',
+	image2: 'img/ph1-2.jpg',
+	displacementImage: 'img/diss.png',
 	imagesRatio: 0.7
 })
 
 new hoverEffect({
 	parent: document.querySelector('.distortion-ph4'),
 	intensity: 0.2,
-	image1: '../img/ph2.jpg',
-	image2: '../img/ph2-2.jpg',
-	displacementImage: '../img/ramen.jpg',
+	image1: 'img/ph2.jpg',
+	image2: 'img/ph2-2.jpg',
+	displacementImage: 'img/ramen.jpg',
 	imagesRatio: 1.2
 })
 new hoverEffect({
 	parent: document.querySelector('.distortion-ph2'),
 	intensity: 0.2,
-	image1: '../img/ph3.jpg',
-	image2: '../img/ph3-2.jpg',
-	displacementImage: '../img/stripe1.png',
+	image1: 'img/ph3.jpg',
+	image2: 'img/ph3-2.jpg',
+	displacementImage: 'img/stripe1.png',
 	imagesRatio: 0.7
 })
 new hoverEffect({
 	parent: document.querySelector('.distortion-ph3'),
 	intensity: 0.2,
-	image1: '../img/ph4.jpg',
-	image2: '../img/ph4-2.jpg',
-	displacementImage: '../img/fluid.jpg',
+	image1: 'img/ph4.jpg',
+	image2: 'img/ph4-2.jpg',
+	displacementImage: 'img/fluid.jpg',
 	imagesRatio: 1.2
 })
